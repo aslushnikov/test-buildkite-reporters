3a86c6b4ba61f72e80b58ab9ce948c32ef6d08c9
Andrey Lushnikov <aslushnikov@gmail.com>
2024-06-10 18:25:24 -0700

    fix config

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
-  reporters: [
-    ['line'],
-    ['buildkite-test-collector/playwright/reporter'],
+  reporter: [
+    ['dot'],
+    // ['buildkite-test-collector/playwright/reporter'],
   ],
 });
--- tests/foo.spec.ts
+++ tests/foo.spec.ts
@@ -1,5 +1,11 @@
 import { test, expect } from '@playwright/test';
 
-test('should work', async () => {
-  expect(1).toBe(1);
+for (let i = 0; i < 1000; ++i) {
+  test(`should work #${i}`, async () => {
+    expect(1).toBe(1);
+  });
+}
+
+test(`is actually failing`, () => {
+  expect(1).toBe(2);
 });
