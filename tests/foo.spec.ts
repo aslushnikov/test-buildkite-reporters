baea55cf12db84054dcfa5b3be26c92d073915e8
Andrey Lushnikov <aslushnikov@gmail.com>
2024-06-10 18:41:00 -0700

    skip flaking tests as well

--- tests/foo.spec.ts
+++ tests/foo.spec.ts
@@ -10,6 +10,6 @@ test.skip(`is actually failing`, () => {
   expect(1).toBe(2);
 });
 
-test(`this is flaking`, () => {
+test.skip(`this is flaking`, () => {
   expect(test.info().retry).toBe(1);
 });
