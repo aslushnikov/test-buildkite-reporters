4e546a80e0d09e46c2542c322a473b118c096731
Andrey Lushnikov <aslushnikov@gmail.com>
2024-07-01 11:22:42 +0100

    integrate currents

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,9 +1,18 @@
 import { defineConfig } from '@playwright/test';
+import { CurrentsConfig, currentsReporter } from "@currents/playwright";
+
+const currentsConfig: CurrentsConfig = {
+  ciBuildId: process.env.CURRENTS_CI_BUILD_ID, // 📖 https://currents.dev/readme/guides/ci-build-id
+  recordKey: process.env.CURRENTS_RECORD_KEY, // 📖 https://currents.dev/readme/guides/record-key
+  projectId: "0aqaxy", // get one at https://app.currents.dev
+};
 
 export default defineConfig({
   retries: 3,
   reporter: [
     ['line'],
     ['buildkite-test-collector/playwright/reporter'],
+    ["allure-playwright"],
+    [currentsReporter(currentsConfig)],
   ],
 });
